ca76258b41540f42242b75c99f3db51ffeaf2435
Lautaro Fernandez Szutner <Lautaroszutner@gmail.com>
2024-01-04 21:18:57 -0700

    agrego js y estilos

---
+++ js/main.js
@@ -0,0 +1,27 @@
+let uno = document.getElementById('uno');
+let dos = document.getElementById('dos');
+let tres = document.getElementById('tres');
+let cuatro = document.getElementById('cuatro');
+let cinco = document.getElementById('cinco');
+
+let colores = function (id) {
+  let numeroColor = [];
+
+  for (let i = 1; i <= 3; i++) {
+    let random = Math.floor(Math.random() * 255) + 1;
+    random.push;
+    numeroColor.push(random);
+  }
+
+  id.style.backgroundColor = `rgb(${numeroColor[0]},${numeroColor[1]},${numeroColor[2]})`;
+
+  id.innerText = `RGB: ${numeroColor}`;
+};
+
+for (let i = 0; i <= 6; i++) {
+  colores(uno);
+  colores(dos);
+  colores(tres);
+  colores(cuatro);
+  colores(cinco);
+}
